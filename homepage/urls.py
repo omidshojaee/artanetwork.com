050afdd6e4c65bedcb647f018de492e3bbad760b
Omid Shojaee <omid.shojaee@gmail.com>
2024-11-22 19:16:30 +0330

    Add homepage view and URL configuration with initial template

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for project project.
+URL configuration for homepage app.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -14,21 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import include, path
 
-from accounts.forms import AuthenticationForm
-from accounts.views import PasswordChangeView
+from django.urls import path
 
-admin.site.login_form = AuthenticationForm
+from . import views
+
+app_name = 'homepage'
 
 urlpatterns = [
-    path('admin/password_change/', PasswordChangeView.as_view(), name='password_change'),
-    path('admin/', admin.site.urls),
-    path('', include('homepage.urls', namespace='homepage')),
+    path('', views.HomepageView.as_view(), name='index'),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
