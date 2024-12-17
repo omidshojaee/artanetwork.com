c2919bc4a17e157e27e6a0fcc336167c74509477
Omid Shojaee <omid.shojaee@gmail.com>
2024-12-17 13:27:56 +0330

    Add Brand model and admin interface with translations

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,35 @@
+from django import forms
 from django.contrib import admin
+from django.utils.html import mark_safe
+from django.utils.translation import gettext_lazy as _
+
+from .models import Brand
 
 # Register your models here.
+
+
+class BrandAdminForm(forms.ModelForm):
+    class Meta:
+        model = Brand
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.instance and self.instance.logo:
+            self.fields['logo'].help_text = mark_safe(
+                f'<img src="{self.instance.logo.url}" alt="{self.instance.name}" style="width: 100px; height: auto; margin-top: 10px;" />'
+            )
+
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    form = BrandAdminForm
+    list_display = ('name', 'logo_thumbnail')
+    search_fields = ('name',)
+
+    def logo_thumbnail(self, obj):
+        return mark_safe(
+            f'<img src="{obj.logo.url}" alt="{obj.name}" style="width: 100px; height: auto;" />'
+        )
+
+    logo_thumbnail.short_description = _('logo thumbnail')
--- products/models.py
+++ products/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+
+class Brand(models.Model):
+    name = models.CharField(_('brand name'), max_length=100)
+    logo = models.ImageField(
+        _('brand logo'), upload_to='brands/', null=True, blank=True
+    )
+
+    class Meta:
+        verbose_name = _('brand')
+        verbose_name_plural = _('brands')
+
+    def __str__(self):
+        return self.name
